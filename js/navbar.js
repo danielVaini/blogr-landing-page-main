be5fe3bfc203dc2a6cd92c3ea643cbf4a456fd93
Daniel Vaini <dnlvaini@gmail.com>
2021-03-21 23:08:05 -0300

    feat: Creatin dropdown event

--- js/navbar.js
+++ js/navbar.js
@@ -1,2 +1,37 @@
 const dropdown = document.querySelectorAll('.dropdown');
 const dropmenu = document.querySelectorAll('.drop-menu');
+
+dropdown.forEach(item => {
+  item.addEventListener('click', () => {
+    if(dropdown[0] === item) {
+      if(dropmenu[0].style.display === 'none'){
+        dropmenu[0].style.display = 'flex'
+        dropmenu[1].style.display = 'none'
+        dropmenu[2].style.display = 'none'
+      }else {
+        dropmenu[0].style.display = 'none'
+
+      }
+    }
+    if(dropdown[1] === item) {
+      if(dropmenu[1].style.display === 'none'){
+        dropmenu[1].style.display = 'flex'
+        dropmenu[0].style.display = 'none'
+        dropmenu[2].style.display = 'none'
+      }else {
+        dropmenu[1].style.display = 'none'
+
+      }
+    }
+    if(dropdown[2] === item) {
+      if(dropmenu[2].style.display === 'none'){
+        dropmenu[2].style.display = 'flex'
+        dropmenu[0].style.display = 'none'
+        dropmenu[1].style.display = 'none'
+      }else {
+        dropmenu[2].style.display = 'none'
+
+      }
+    }
+  })
+})
